22e597e228add2c8d0de11435f9060b4c6de3071
Pakizamutallim <127186291+Pakizamutallim@users.noreply.github.com>
2023-05-23 12:03:49 +0400

    Add files via upload

---
+++ 23.05.23/assets/js/main.js
@@ -0,0 +1,17 @@
+const button = document.getElementById("btn")
+
+  button.addEventListener("click", function(){
+    fetch('https://6469bba8183682d614455b66.mockapi.io/users', {
+        method: "POST", 
+        headers: {
+        "Content-Type": "application/json",
+        },
+        body: {
+            name: document.querySelector(".name").value,
+            image: document.querySelector(".image").value,
+            info: document.querySelector(".info").value,
+            date: document.querySelector(".date").value 
+        }
+    }).then(res=>console.log(res))
+
+})
